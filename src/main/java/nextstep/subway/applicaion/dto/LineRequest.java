ba694bd5a7d96f8e4084332079fb18217cdaec7b
sjhello <sjhello235@gmail.com>
2023-04-18 18:45:14 +0900

    refactor: DTO lombok

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -1,34 +1,16 @@
 package nextstep.subway.applicaion.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class LineRequest {
     private String name;
     private String color;
     private Long upStationId;
     private Long downStationId;
     private int distance;
-
-    public String getName() {
-        return name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public Long getUpStationId() {
-        return upStationId;
-    }
-
-    public Long getDownStationId() {
-        return downStationId;
-    }
-
-    public int getDistance() {
-        return distance;
-    }
-
-    public LineRequest() {
-    }
 
     public LineRequest(String name, String color) {
         this.name = name;
--- src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -1,40 +1,17 @@
 package nextstep.subway.applicaion.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class LineResponse {
     private Long id;
     private String name;
     private String color;
     private List<StationResponse> stations;
     private int distance;
-
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations, int distance) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-        this.stations = stations;
-        this.distance = distance;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public List<StationResponse> getStations() {
-        return stations;
-    }
-
-    public int getDistance() {
-        return distance;
-    }
 }
 
