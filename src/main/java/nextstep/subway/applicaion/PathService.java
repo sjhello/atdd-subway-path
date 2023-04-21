d2bf1e8615a1f5bf0df7641c59db4713b7ec5192
sjhello <sjhello235@gmail.com>
2023-04-21 18:07:54 +0900

    temporary commit

--- src/main/java/nextstep/subway/applicaion/PathService.java
+++ src/main/java/nextstep/subway/applicaion/PathService.java
@@ -1,13 +1,47 @@
 package nextstep.subway.applicaion;
 
+import lombok.RequiredArgsConstructor;
 import nextstep.subway.applicaion.dto.PathRequest;
 import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class PathService {
 
+    private final StationService stationService;
+
+    private final LineRepository lineRepository;
+
     public PathResponse getPath(PathRequest pathRequest) {
+        Station source = stationService.findById(pathRequest.getSource());
+        Station target = stationService.findById(pathRequest.getTarget());
+
+        List<Line> lines = lineRepository.findAll();
+
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+        for (Line line : lines) {
+            line.getStations().stream().forEach(graph :: addVertex);
+            line.getSections().stream().forEach(section ->
+                    graph.setEdgeWeight(
+                            graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance())
+            );
+        }
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        GraphPath shortestPath = dijkstraShortestPath.getPath(source, target);
+
         return new PathResponse();
     }
 }
---
+++ src/main/java/nextstep/subway/domain/PathFinder.java
@@ -0,0 +1,12 @@
+package nextstep.subway.domain;
+
+import java.util.List;
+
+public class PathFinder {
+
+    private final List<Line> lines;
+
+    public PathFinder(List<Line> lines) {
+        this.lines = lines;
+    }
+}
