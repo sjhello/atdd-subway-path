64bff92561490616312213cd152807498cb8e33d
sjhello <sjhello235@gmail.com>
2023-04-25 11:14:15 +0900

    test: 연결되어 있지 않은 경로 테스트

--- src/test/java/nextstep/study/JgraphtTest.java
+++ src/test/java/nextstep/study/JgraphtTest.java
@@ -54,4 +54,25 @@ class JgraphtTest {
                     assertThat(it.getVertexList()).endsWith(target);
                 });
     }
+
+    @Test
+    void notConnection() {
+        String source = "v1";
+        String target = "v5";
+
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        graph.addVertex("v4");
+        graph.addVertex("v5");
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v4", "v5"), 100);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        List<String> shortestPath = dijkstraShortestPath.getPath(source, target).getVertexList();
+
+        assertThat(shortestPath.size()).isEqualTo(3);
+    }
 }
