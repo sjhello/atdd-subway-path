929e9fdfdbcd0fcb503dfa03896f986b64c4b8e0
sjhello <sjhello235@gmail.com>
2023-04-25 15:08:57 +0900

    test: 인수 테스트 실패 케이스
     - 출발지와 목적지가 같은 경우
     - 출발지와 목적지가 연결되어 있지 않은 경우

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -1,7 +1,11 @@
 package nextstep.subway.acceptance;
 
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBodyExtractionOptions;
+import nextstep.subway.domain.path.PathErrorCode;
+import nextstep.subway.domain.path.PathException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -22,9 +26,12 @@ class PathAcceptanceTest extends AcceptanceTest{
     private Long 강남역;
     private Long 양재역;
     private Long 남부터미널역;
+    private Long 길동역;
+    private Long 강동역;
     private Long 이호선;
     private Long 신분당선;
     private Long 삼호선;
+    private Long 오호선;
 
 
     /**
@@ -47,11 +54,14 @@ class PathAcceptanceTest extends AcceptanceTest{
         교대역 = 지하철역_생성_요청_ID("교대역");
         강남역 = 지하철역_생성_요청_ID("강남역");
         양재역 = 지하철역_생성_요청_ID("양재역");
+        길동역 = 지하철역_생성_요청_ID("길동역");
+        강동역 = 지하철역_생성_요청_ID("길동역");
         남부터미널역 = 지하철역_생성_요청_ID("남부터미널역");
 
         이호선 = 지하철_노선_생성_요청_ID(createLineCreateParams("2호선", "green", 교대역, 강남역, 10));
         신분당선 = 지하철_노선_생성_요청_ID(createLineCreateParams("신분당선", "red", 강남역, 양재역, 10));
         삼호선 = 지하철_노선_생성_요청_ID(createLineCreateParams("3호선", "orange", 교대역, 남부터미널역, 2));
+        오호선 = 지하철_노선_생성_요청_ID(createLineCreateParams("5호선", "purple", 길동역, 강동역, 17));
 
         지하철_노선에_지하철_구간_생성_요청(삼호선, createSectionCreateParams(남부터미널역, 양재역, 3));
     }
@@ -69,5 +79,35 @@ class PathAcceptanceTest extends AcceptanceTest{
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getInt("distance")).isEqualTo(5);
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
+    }
+
+    /**
+     * When 목적지와 도착지가 같으면
+     * Then 경로조회에 실패한다
+     * */
+    @Test
+    void 목적지와_도착지가_같으면_경로조회에_실패한다() {
+        // when
+        ExtractableResponse<Response> response = 경로_조회_요청(교대역, 교대역);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(response.body().jsonPath().getString("errorCode")).isEqualTo(PathErrorCode.SAME_STATION.getCode());
+        assertThat(response.body().jsonPath().getString("message")).isEqualTo(PathErrorCode.SAME_STATION.getMessage());
+    }
+
+    /**
+     * When 목적지와 도착지가 연결되어 있지 않으면
+     * Then 경로조회에 실패한다
+     * */
+    @Test
+    void 목적지와_도착지가_연결되어_있지_않으면_경로조회에_실패한다() {
+        // when
+        ExtractableResponse<Response> response = 경로_조회_요청(교대역, 길동역);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(response.body().jsonPath().getString("errorCode")).isEqualTo(PathErrorCode.NOT_CONNECTION.getCode());
+        assertThat(response.body().jsonPath().getString("message")).isEqualTo(PathErrorCode.NOT_CONNECTION.getMessage());
     }
 }
