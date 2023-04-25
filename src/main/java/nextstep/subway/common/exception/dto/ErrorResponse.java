e3558883c72933c8aa57703b849f2687a09e04b9
sjhello <sjhello235@gmail.com>
2023-04-25 15:09:41 +0900

    feat: PathException ExceptionHandler method 작성

---
+++ src/main/java/nextstep/subway/common/exception/dto/ErrorResponse.java
@@ -0,0 +1,15 @@
+package nextstep.subway.common.exception.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+
+    private String errorCode;
+
+    private String message;
+}
--- src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
+++ src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
@@ -1,5 +1,8 @@
 package nextstep.subway.ui;
 
+import nextstep.subway.common.exception.dto.ErrorResponse;
+import nextstep.subway.domain.path.PathBadRequestException;
+import nextstep.subway.domain.path.PathException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,5 +13,11 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Void> handleIllegalArgsException(DataIntegrityViolationException e) {
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(PathBadRequestException.class)
+    public ResponseEntity<ErrorResponse> handlePathException(PathException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getCode(), e.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 }
